13f83b29a6fadfd95fe6117eccc752de152257a2
Juan Pablo M. <pablochileidh@gmail.com>
2024-01-17 22:44:09 -0300

    terminado ejercicio 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-const colorFondo = document.body.style.backgroundColor = 'LightGoldenrodYellow';
+bandera = 0;
+
+function bordeRojo() {
+   
+    if (bandera == 0){
+        document.getElementById("uno").
+        className = "borderojo img-fluid";
+        bandera = 1
+    }else if(bandera == 1){
+        document.getElementById("uno").
+        className = " img-fluid";
+        bandera = 0;
+    }
+
+    
+ 
+  }
